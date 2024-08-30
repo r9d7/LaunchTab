2ce18cbaba330d9d91cd49071f9addfeada4a5d9
Radu Dascalu <radu.dascalu@pm.me>
2024-08-30 22:03:31 +0100

    WIP styles

--- uno.config.ts
+++ uno.config.ts
@@ -7,6 +7,27 @@ import {
 } from "unocss";
 
 export default defineConfig({
-  presets: [presetUno(), presetWebFonts({ provider: "bunny" })],
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "bunny",
+      fonts: { sans: { name: "Inter", weights: ["400", "700"] } },
+    }),
+  ],
+  theme: {
+    colors: {
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+    },
+  },
   transformers: [transformerVariantGroup(), transformerDirectives()],
 });
